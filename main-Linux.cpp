6c6e5aa616276dffe05909c579fa546397478a45
Isaac Estolano <srlestolano@gmail.com>
2026-09-04 09:24:40 -0300

    Create function movi

--- main-Linux.cpp
+++ main-Linux.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <termios.h>
 #include <unistd.h>
-#define order 15
-#define line 15
-#define collum 15
+#define order 14
+#define line 14
+#define collum 14
 #define line2 9
 #define collum2 12
 #define hollow 0
@@ -21,7 +21,17 @@ void defineCoordinates(int pli,int pco){
             pli = 10;
             pco = 2;
 }
-
+void reset(int mapSelection,int map_reset[][order],int map[][order],int pli,int pco){
+       if (mapSelection == 1) {
+                for (int i = 0; i < line; i++) {
+                    for (int j = 0; j < collum; j++) {
+                        map[i][j] = map_reset[i][j];
+                    }
+                }
+                pli = 10;
+                pco = 2;
+            }
+}
 bool win(bool venceu){
           if (venceu == true) {
             cout << "\n==========================================" << endl;
@@ -30,7 +40,7 @@ bool win(bool venceu){
         }
     return false;
 }
-void mapRotationNormal(int map[][collum],int pli,int pco){
+void mapRotationNormal(int map[][collum],int pli,int pco,int &contRotates){
    int N = order; // Tamanho da map NxN
 for (int i = 0; i < N / 2; i++) {
     for (int j = i; j < N - i - 1; j++) {
@@ -41,145 +51,14 @@ for (int i = 0; i < N / 2; i++) {
         map[j][N - 1 - i] = temp;
     }
 }
+    contRotates ++;
     }
 
-void mapRotationAnti(int map[][collum],int pli,int pco){
+void mapRotationAnti(int map[][collum],int pli,int pco,int &contRotates){
 
 }
-
-int getch(void) {
-    int ch;
-    struct termios oldt, newt;
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-    ch = getchar();
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    return ch;
-}
-
-int main() {
-    printf("\033[?25l");
-
-    int pli, pco;
-    int options = 0;
-    int mapSelection = 0;
-    char x;
-    bool venceu = false;
-    bool desistiu = false;
-    int caixasSoltas = 0;
-    int contRotates=0;
-    int contMov=0;
-    int map[line][collum] = {
-        {3,3,1,1,1,1,1,3,3,3,3,3,3,3,3},
-        {3,3,1,0,0,0,1,3,3,3,3,3,3,3,3},
-        {3,3,1,0,0,0,1,1,1,1,1,1,3,3,3},
-        {3,3,1,0,0,0,0,1,0,0,0,1,3,3,3},
-        {3,3,1,1,0,0,0,1,0,0,0,1,3,3,3},
-        {3,1,1,1,0,0,0,0,0,0,0,1,3,3,3},
-        {1,1,1,1,0,0,1,1,1,0,0,1,3,3,3},
-        {1,1,1,1,0,0,1,3,1,0,2,1,3,3,3},
-        {1,0,0,4,0,0,1,3,1,0,2,1,3,3,3},
-        {1,0,0,4,4,0,1,3,1,0,2,1,3,3,3},
-        {1,0,5,0,0,0,1,3,1,1,1,1,3,3,3},
-        {1,1,1,1,1,1,1,3,3,3,3,3,3,3,3},
-        {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3},
-        {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3},
-        {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}
-    };
-
-    int map_reset[line][collum] = {
-        {3,3,1,1,1,1,1,3,3,3,3,3,3,3,3},
-        {3,3,1,0,0,0,1,3,3,3,3,3,3,3,3},
-        {3,3,1,0,0,0,1,1,1,1,1,1,3,3,3},
-        {3,3,1,0,0,0,0,1,0,0,0,1,3,3,3},
-        {3,3,1,1,0,0,0,1,0,0,0,1,3,3,3},
-        {3,1,1,1,0,0,0,0,0,0,0,1,3,3,3},
-        {1,1,1,1,0,0,1,1,1,0,0,1,3,3,3},
-        {1,1,1,1,0,0,1,3,1,0,2,1,3,3,3},
-        {1,0,0,4,0,0,1,3,1,0,2,1,3,3,3},
-        {1,0,0,4,4,0,1,3,1,0,2,1,3,3,3},
-        {1,0,5,0,0,0,1,3,1,1,1,1,3,3,3},
-        {1,1,1,1,1,1,1,3,3,3,3,3,3,3,3},
-        {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3},
-        {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3},
-        {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}
-    };
-
-    cout << "\n1-Autores  2-Orientacoes do jogo  3-Jogar\n";
-    cin >> options;
-
-    if (options == 1) {
-        cout << "Autores: Isaac Estolano\n";
-        cout << "\nPressione qualquer tecla para continuar...";
-        getch();
-        cout << "\n1-Autores  2-Orientacoes do jogo  3-Jogar\n";
-        cin >> options;
-    }
-
-    if (options == 2) {
-        cout << "==================================================" << endl;
-        cout << "              ORIENTACOES DO JOGO                " << endl;
-        cout << "==================================================" << endl;
-        cout << endl;
-        cout << "OBJETIVO:" << endl;
-        cout << "Posicione todas as caixas ($) sobre os alvos (-)." << endl;
-        cout << "Quando uma caixa chega ao alvo ela vira %." << endl;
-        cout << "O nivel esta completo quando nao houver mais $ no mapa." << endl;
-        cout << endl;
-        cout << "CONTROLES:" << endl;
-        cout << "  W - Mover para cima" << endl;
-        cout << "  S - Mover para baixo" << endl;
-        cout << "  A - Mover para esquerda" << endl;
-        cout << "  D - Mover para direita" << endl;
-        cout << "  R - Reiniciar o mapa atual" << endl;
-        cout << "Nao e necessario pressionar Enter." << endl;
-        cout << endl;
-        cout << "LEGENDA DO MAPA:" << endl;
-        cout << "  @  Jogador" << endl;
-        cout << "  ?  Jogador sobre um alvo" << endl;
-        cout << "  $  Caixa" << endl;
-        cout << "  %  Caixa posicionada no alvo (correta)" << endl;
-        cout << "  -  Alvo (destino de uma caixa)" << endl;
-        cout << "  &  Parede" << endl;
-        cout << "     Espaco vazio / area externa" << endl;
-        cout << endl;
-        cout << "REGRAS FUNDAMENTAIS:" << endl;
-        cout << "  - O jogador so consegue empurrar caixas, nunca puxa-las." << endl;
-        cout << "  - So e possivel empurrar uma caixa se o espaco atras" << endl;
-        cout << "    dela estiver livre (vazio ou alvo)." << endl;
-        cout << "  - Nao e possivel empurrar duas caixas ao mesmo tempo." << endl;
-        cout << "  - Caixas nao podem atravessar paredes." << endl;
-        cout << endl;
-        cout << "DICAS DE ESTRATEGIA:" << endl;
-        cout << "  - Planeje antes de agir: um movimento errado pode" << endl;
-        cout << "    deixar o nivel sem solucao." << endl;
-        cout << "  - Caixas encostadas em cantos ou ao longo de paredes" << endl;
-        cout << "    sem alvo proximo ficam travadas para sempre." << endl;
-        cout << "  - Observe os alvos e trace o caminho inverso: de onde" << endl;
-        cout << "    a caixa precisa vir para chegar la." << endl;
-        cout << "  - Se travar, reinicie o nivel com R e tente outra abordagem." << endl;
-        cout << endl;
-        cout << "==================================================" << endl;
-        cout << "        Pressione qualquer tecla para jogar...    " << endl;
-        cout << "==================================================" << endl;
-        getch();
-        options = 3;
-    }
-
-    if (options == 3) {
-        cout << "Digite 1 para o mapa map1 e 2 para map2: ";
-        cin >> mapSelection;
-        
-    }
-
-    while (true) {
-
-        system("clear");
-
-        if (mapSelection == 1) {
-            for (int i = 0; i < line; i++) {
+void mapGenerate(int map[][order]){
+    for (int i = 0; i < line; i++) {
                 for (int j = 0; j < collum; j++) {
                     switch (map[i][j]) {
                         case hollow:   
@@ -213,73 +92,21 @@ int main() {
                 }
                 cout << endl;
             }
-            cout << "\n[R] Reiniciar  [G] Desistir" << endl;
-        }
-
-        if (venceu == true) {
-            cout << "\n==========================================" << endl;
-            cout << "        PARABENS! Voce venceu!           " << endl;
-            cout << "==========================================" << endl;
-            break;
-        }
-
-      
-        x = getch();
-
-        // RESET
-        if (x == 'r') {
-            if (mapSelection == 1) {
-                for (int i = 0; i < line; i++) {
-                    for (int j = 0; j < collum; j++) {
-                        map[i][j] = map_reset[i][j];
-                    }
-                }
-                pli = 10;
-                pco = 2;
-            }
-        }
-
-        if (x == 'g') {
-            desistiu = true;
-        }
-        if (mapSelection == 1) {
-            switch (x) {
+}
+void movi(char x,int map[][order],int pli,int pco,int contRotates,int contmovi){
+    switch (x) {
             case 'w':
                 if (map[pli-1][pco] == wall) {
                     break;
                 }
-                if (map[pli-1][pco] == box || map[pli-1][pco] == boxIn) {
-                    if (map[pli-2][pco] == hollow || map[pli-2][pco] == rightPlace) {
-                        if (map[pli-2][pco] == rightPlace) {
-                            map[pli-2][pco] = boxIn;
-                        }
-                        else {
-                            map[pli-2][pco] = box;
-                        }
-                        if (map[pli-1][pco] == boxIn) {
-                            map[pli-1][pco] = playerIn;
-                        }
-                        else {
-                            map[pli-1][pco] = player;
-                        }
-                        if (map[pli][pco] == playerIn) {
-                            map[pli][pco] = rightPlace;
-                        }
-                        else {
-                            map[pli][pco] = hollow;
-                        }
-                        pli--;
-                    }
-                    break;
-                }
-                if (map[pli-1][pco] == rightPlace) {
+                if (map[pli-1][pco] == lever) {
                     map[pli-1][pco] = playerIn;
                 }
                 else {
                     map[pli-1][pco] = player;
                 }
                 if (map[pli][pco] == playerIn) {
-                    map[pli][pco] = rightPlace;
+                    map[pli][pco] = lever;
                 }
                 else {
                     map[pli][pco] = hollow;
@@ -416,19 +243,208 @@ int main() {
                 pco++;
                 break;
                 case 'q':
-                    mapRotationAnti(map,pli,pco);
+                    mapRotationAnti(map,pli,pco,contRotates);
                     break;
                 case 'e':
-                    mapRotationNormal(map,pli,pco);
+                    mapRotationNormal(map,pli,pco,contRotates);
             }
-        }
+}
+int getch(void) {
+    int ch;
+    struct termios oldt, newt;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return ch;
+}
+
+int main() {
+    printf("\033[?25l");
+
+    int pli, pco;
+    int options = 0;
+    int mapSelection = 0;
+    char x;
+    bool venceu = false;
+    bool desistiu = false;
+    int caixasSoltas = 0;
+    int contRotates=0;
+    int contMovi=0;
+    //d s s s s d s e e e d d d d w w a w a
+    int map[order][order] = {
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    {1,0,0,0,1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,1,0,1,6,0,0,1},
+    {1,0,0,0,0,0,2,0,1,0,0,0,0,1},
+    {1,0,0,0,0,1,1,0,1,1,0,0,1,1},
+    {1,0,0,0,0,0,0,0,0,0,1,0,0,1},
+    {1,0,0,1,0,0,3,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,1,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,1,1,4,0,0,0,1,1},
+    {1,0,0,0,0,0,0,7,0,0,0,1,0,1},
+    {1,0,0,0,1,0,0,1,0,0,1,5,0,1},
+    {1,0,0,1,0,0,0,0,0,1,0,0,0,1},
+    {1,0,0,0,0,0,0,1,0,3,6,1,0,1},
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+ int mapReset[order][order] = {
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    {1,0,0,0,1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,1,0,1,6,0,0,1},
+    {1,0,0,0,0,0,2,0,1,0,0,0,0,1},
+    {1,0,0,0,0,1,1,0,1,1,0,0,1,1},
+    {1,0,0,0,0,0,0,0,0,0,1,0,0,1},
+    {1,0,0,1,0,0,3,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,1,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,1,1,4,0,0,0,1,1},
+    {1,0,0,0,0,0,0,7,0,0,0,1,0,1},
+    {1,0,0,0,1,0,0,1,0,0,1,5,0,1},
+    {1,0,0,1,0,0,0,0,0,1,0,0,0,1},
+    {1,0,0,0,0,0,0,1,0,3,6,1,0,1},
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+    //w w w w d d d d w d d d e e a a a a
+  int map2[order][order] = {
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,7,0,0,0,0,1,1},
+    {1,0,1,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,1,6,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,1,0,0,1,0,1,0,1},
+    {1,0,0,0,0,0,0,0,1,0,0,0,0,1},
+    {1,0,0,0,1,0,0,0,4,3,0,0,5,1},
+    {1,0,0,0,0,0,0,1,1,1,1,0,0,1},
+    {1,0,0,0,3,0,0,0,0,1,0,0,1,1},
+    {1,0,0,0,1,0,0,0,0,1,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,1,0,6,1},
+    {1,2,0,0,0,0,0,1,0,1,0,0,0,1},
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+  int map2Reset[order][order] = {
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,7,0,0,0,0,1,1},
+    {1,0,1,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,1,6,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,1,0,0,1,0,1,0,1},
+    {1,0,0,0,0,0,0,0,1,0,0,0,0,1},
+    {1,0,0,0,1,0,0,0,4,3,0,0,5,1},
+    {1,0,0,0,0,0,0,1,1,1,1,0,0,1},
+    {1,0,0,0,3,0,0,0,0,1,0,0,1,1},
+    {1,0,0,0,1,0,0,0,0,1,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,1,0,6,1},
+    {1,2,0,0,0,0,0,1,0,1,0,0,0,1},
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+//w w w w w a a w a a a a a e e e w w w w d d d d w d d d s
+int mapHard[order][order] = {
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    {1,1,0,0,1,0,0,0,0,1,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1,0,0,0,1},
+    {1,0,0,0,1,0,0,0,0,0,1,1,0,1},
+    {1,0,0,0,0,0,0,0,0,1,0,0,0,1},
+    {1,0,0,4,0,0,0,0,0,1,0,0,0,1},
+    {1,0,0,0,0,1,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,1,0,0,0,0,1},
+    {1,6,0,0,0,0,0,0,0,0,0,6,0,1},
+    {1,0,0,0,1,0,7,6,0,0,0,0,0,1},
+    {1,0,0,1,0,0,0,1,0,0,2,0,0,1},
+    {1,0,0,0,3,0,1,5,3,1,1,0,3,1},
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+int mapHardReset[order][order] = {
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    {1,1,0,0,1,0,0,0,0,1,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1,0,0,0,1},
+    {1,0,0,0,1,0,0,0,0,0,1,1,0,1},
+    {1,0,0,0,0,0,0,0,0,1,0,0,0,1},
+    {1,0,0,4,0,0,0,0,0,1,0,0,0,1},
+    {1,0,0,0,0,1,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,1,0,0,0,0,1},
+    {1,6,0,0,0,0,0,0,0,0,0,6,0,1},
+    {1,0,0,0,1,0,7,6,0,0,0,0,0,1},
+    {1,0,0,1,0,0,0,1,0,0,2,0,0,1},
+    {1,0,0,0,3,0,1,5,3,1,1,0,3,1},
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+    cout << "\n1-Autores  2-Orientacoes do jogo  3-Jogar\n";
+    cin >> options;
+
+    if (options == 1) {
+        cout << "Autores: Isaac Estolano\n";
+        cout << "\nPressione qualquer tecla para continuar...";
+        getch();
+        cout << "\n1-Autores  2-Orientacoes do jogo  3-Jogar\n";
+        cin >> options;
     }
 
-    if (desistiu == true) {
+    if (options == 2) {
+        cout << "==================================================" << endl;
+        cout << "              ORIENTACOES DO JOGO                " << endl;
+        cout << "==================================================" << endl;
+        cout << "CONTROLES:" << endl;
+        cout << "  W - Mover para cima" << endl;
+        cout << "  S - Mover para baixo" << endl;
+        cout << "  A - Mover para esquerda" << endl;
+        cout << "  D - Mover para direita" << endl;
+        cout << "  R - Reiniciar o mapa atual" << endl;
+        cout << "Nao e necessario pressionar Enter." << endl;
+        cout << endl;
+        getch();
+        options = 3;
+    }
+
+    if (options == 3) {
+        cout << "Digite 1 para o mapa map1 e 2 para map2: ";
+        cin >> mapSelection;
+        
+    }
+
+    while (true) {
+
         system("clear");
-        cout << "\n==========================================" << endl;
-        cout << "     Voce desistiu. Ate a proxima!       " << endl;
-        cout << "==========================================" << endl;
+//Procurar alguma forma de não fazer 3 if para cada mapa
+        if (mapSelection == 1) {
+            mapGenerate(map);
+            defineCoordinates(pli,pco);
+        }
+         if (mapSelection == 2) {
+            mapGenerate(map2);
+            defineCoordinates(pli,pco);
+        }
+         if (mapSelection == 3) {
+            mapGenerate(mapHard);
+            defineCoordinates(pli,pco);
+        }
+        x = getch();
+        movi(x,map,pli,pco,contRotates,contMovi);
+        // RESET
+            if (x == 'r' && mapSelection==1) {
+            reset(mapSelection,mapReset,map,pli,pco);
+        }
+            if (x == 'r' && mapSelection==2) {
+            reset(mapSelection,map2Reset,map,pli,pco);
+        }
+            if (x == 'r' && mapSelection==3) {
+            reset(mapSelection,mapHardReset,map,pli,pco);
+        }
+            if (x == 'g') {
+            return false;
+        }
+            if (mapSelection) {
+            movi(x,map,pli,pco,contRotates,contMovi);
+        }
     }
     return 0;
+         cout << "\n==========================================" << endl;
+            cout << "        PARABENS! Voce venceu!           " << endl;
+            cout << "==========================================" << endl;
 }
+//Passagem por valor nao precisa de backup das matrizes
+//Procurar alguma forma de não fazer 3 if para cada mapa
+//Adicionar as alavancas no mapgenerate
